ea11dd8fcc106db22fc540429d528041c6693f5b
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-09-25 16:40:19 +0530

    improve swap performance

--- src/server.ts
+++ src/server.ts
@@ -3,63 +3,30 @@ import { searchToken } from "./utils/search-token";
 import { promises as fs } from "fs"; // Using fs.promises to read the file
 import path from "path";
 import cors from "cors";
-import {
-  WRAP_NEAR_CONTRACT_ID,
-  ftGetStorageBalance,
-  nearDepositTransaction,
-  nearWithdrawTransaction,
-  percentLess,
-  registerAccountOnToken,
-  scientificNotationToString,
-} from "@ref-finance/ref-sdk";
 import Big from "big.js";
 import * as dotenv from "dotenv";
+import {
+  BalanceResp,
+  SmartRouter,
+  Token,
+} from "./utils/interface";
+import { swapFromServer, unWrapNear, wrapNear } from "./utils/lib";
 dotenv.config();
 
 const app = express();
 const port = 3003;
 
-interface BalanceResp {
-  amount: number;
-  contract: string;
-  symbol: string;
-}
-
-interface Token {
-  id: string;
-  name: string;
-  symbol: string;
-  icon: string;
-  price: string;
-  balance: string;
-  parsedBalance: string;
-  decimals: number;
-}
-
-interface Pool {
-  pool_id: string;
-  token_in: string;
-  token_out: string;
-  amount_in: string;
-  min_amount_out: string;
-}
-
-interface Routes {
-  pools: Pool[];
-  amount_in: string;
-  min_amount_out: string;
-}
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/token-metadata", async (req: Request, res: Response) => {
   try {
-    const { token } = req.query;
+    const { token } = req.query as { token: string };
     const filePath = path.join(__dirname, "tokens.json");
     const data = await fs.readFile(filePath, "utf-8");
     const tokens: Record<string, Token> = JSON.parse(data);
-    res.json(tokens[token as string]);
+    res.json(tokens[token]);
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -74,185 +41,145 @@ app.get("/whitelist-tokens", async (req: Request, res: Response) => {
     const filePath = path.join(__dirname, "tokens.json");
     const data = await fs.readFile(filePath, "utf-8");
     const tokens: Record<string, Token> = JSON.parse(data);
-    let userBalances: BalanceResp[] = [];
 
-    // Fetch prices and balances of the tokens
-    if (account) {
-      const headers: HeadersInit = {
-        "Content-Type": "application/json",
-      };
-      const apiKey = process.env.PIKESPEAK_KEY;
-      if (apiKey) {
-        headers["x-api-key"] = apiKey;
-      }
-      userBalances = await (
-        await fetch(`https://api.pikespeak.ai/account/balance/${account}`, {
+    // Fetch prices and balances concurrently
+    const fetchBalancesPromise = account
+      ? fetch(`https://api.pikespeak.ai/account/balance/${account}`, {
           method: "GET",
-          headers,
-        })
-      ).json();
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": process.env.PIKESPEAK_KEY || "",
+          },
+        }).then((res) => res.json())
+      : Promise.resolve([]);
 
-      userBalances = userBalances.filter( i => i.symbol !== "NEAR [Storage]")
-    }
-
-    const tokenIds = Object.keys(tokens);
-    const tokenMetadataPromises = tokenIds.map((id) => {
+    const fetchTokenPricePromises = Object.keys(tokens).map((id) => {
       return fetch(
         `https://api.ref.finance/get-token-price?token_id=${
           id === "near" ? "wrap.near" : id
         }`
       )
-        .then((res) => {
-          if (!res.ok) {
-            // Handle non-200 responses
-            throw new Error(
-              `Failed to fetch for token_id ${id}: ${res.statusText}`
-            );
-          }
-          return res.json().catch(() => {
-            // Handle invalid JSON responses
-            throw new Error(`Invalid JSON response for token_id ${id}`);
-          });
-        })
+        .then((res) => res.json())
         .catch((err) => {
-          // Handle fetch errors
-          console.error(err.message);
-          return { price: "N/A" }; // Return null or handle appropriately
+          console.error(
+            `Error fetching price for token_id ${id}: ${err.message}`
+          );
+          return { price: "N/A" }; // Default value for failed fetches
         });
     });
 
-    // Wait for all metadata fetches to complete
-    const tokenMetadataResponses = await Promise.all(tokenMetadataPromises);
+    // Wait for both balances and token prices to resolve
+    const [userBalances, tokenPrices] = await Promise.all([
+      fetchBalancesPromise,
+      Promise.all(fetchTokenPricePromises),
+    ]);
 
-    // Update tokens with fetched prices and balances
-    tokenIds.forEach((id, index) => {
-      const resp = tokenMetadataResponses[index];
-      tokens[id].price = resp?.price ?? "0";
-      tokens[id].parsedBalance =
-        userBalances.find((i: BalanceResp) => i.contract.toLowerCase() === id)?.amount.toString() ??
-        "0";
-      tokens[id].balance = Big(tokens[id].parsedBalance)
-        .mul(Big(10).pow(tokens[id]?.decimals))
+    const filteredBalances = userBalances.filter(
+      (i: any) => i.symbol !== "NEAR [Storage]"
+    );
+
+    // Map over tokens to include only the required fields
+    const simplifiedTokens = Object.keys(tokens).map((id, index) => {
+      const token = tokens[id];
+      const priceData = tokenPrices[index];
+
+      const parsedBalance =
+        filteredBalances
+          .find((i: BalanceResp) => i.contract.toLowerCase() === id)
+          ?.amount.toString() || "0";
+
+      const balance = Big(parsedBalance)
+        .mul(Big(10).pow(token.decimals))
         .toFixed(4);
+
+      return {
+        id,
+        decimals: token.decimals,
+        parsedBalance,
+        balance,
+        price: priceData.price !== "N/A" ? Big(priceData.price ?? "").toFixed(4)  : priceData.price,
+        symbol: token.symbol,
+        name: token.name,
+        icon: token.icon,
+      };
     });
 
-    const sortedTokens = Object.values(tokens).sort((a, b) => {
-      const balanceA = parseFloat(a.parsedBalance);
-      const balanceB = parseFloat(b.parsedBalance);
-      return balanceB - balanceA; // Sort in descending order
-    });
+    // Return sorted tokens based on balance (optional step)
+    const sortedTokens = simplifiedTokens.sort(
+      (a, b) => parseFloat(b.parsedBalance) - parseFloat(a.parsedBalance)
+    );
 
     return res.json(sortedTokens);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return res.status(500).json({
-      error: "An error occurred while fetching whitelisted tokens",
+      error: "An error occurred while fetching tokens and balances",
     });
   }
 });
 
 app.get("/swap", async (req: Request, res: Response) => {
-  const { accountId, tokenIn, tokenOut, amountIn, slippage } = req.query;
+  const { accountId, tokenIn, tokenOut, amountIn, slippage } = req.query as {
+    accountId: string;
+    tokenIn: string;
+    tokenOut: string;
+    amountIn: string;
+    slippage: string;
+  };
 
   try {
-    const tokenInData = await searchToken(tokenIn as string);
-    const tokenOutData = await searchToken(tokenOut as string);
+    const isWrapNearInputToken = tokenIn === "wrap.near";
+    const isWrapNearOutputToken = tokenOut === "wrap.near";
+    const tokenInData = await searchToken(tokenIn);
+    const tokenOutData = await searchToken(tokenOut);
 
     if (!tokenInData || !tokenOutData) {
-      return {
+      return res.status(404).json({
         error: `Unable to find token(s) tokenInData: ${tokenInData?.name} tokenOutData: ${tokenOutData?.name}`,
-      };
+      });
     }
 
+    // (un)wrap NEAR
     if (tokenInData.id === tokenOutData.id) {
-      if (tokenInData.id === WRAP_NEAR_CONTRACT_ID) {
-        return {
-          error:
-            "This endpoint does not support wrapping / unwrap NEAR directly",
-        };
+      if (isWrapNearInputToken && !isWrapNearOutputToken) {
+        return res.json({
+          transactions: [await unWrapNear({ amountIn })],
+          outEstimate: amountIn,
+        });
       }
-      return { error: "TokenIn and TokenOut cannot be the same" };
+
+      if (!isWrapNearInputToken && isWrapNearOutputToken) {
+        return res.json({
+          transactions: [await wrapNear({ amountIn, accountId })],
+          outEstimate: amountIn,
+        });
+      }
     }
 
-    const sendAmount = Big(amountIn as string)
+    const sendAmount = Big(amountIn)
       .mul(Big(10).pow(tokenInData.decimals))
       .toFixed();
-    const swapRes = await (
+    const swapRes: SmartRouter = await (
       await fetch(
         `https://smartrouter.ref.finance/findPath?amountIn=${sendAmount}&tokenIn=${tokenInData.id}&tokenOut=${tokenOutData.id}&pathDeep=3&slippage=${slippage}`
       )
     ).json();
-    const routes = swapRes.result_data.routes.flatMap(
-      (route: Routes) => route.pools
-    );
 
     const receiveAmount = Big(swapRes.result_data.amount_out)
       .div(Big(10).pow(tokenOutData.decimals))
       .toFixed();
 
-    const transactions = [];
-    const tokenInStorage = await ftGetStorageBalance(
-      tokenInData.id,
-      accountId as string
-    );
-    const tokenOutStorage = await ftGetStorageBalance(
-      tokenOutData.id,
-      accountId as string
-    );
-
-    if (tokenInData.id === WRAP_NEAR_CONTRACT_ID) {
-      transactions.unshift(nearDepositTransaction(amountIn as string));
-    }
-
-    transactions.push({
-      receiverId: tokenInData.id,
-      functionCalls: [
-        {
-          methodName: "ft_transfer_call",
-          args: {
-            receiver_id: "v2.ref-finance.near",
-            amount: sendAmount,
-            msg: JSON.stringify({
-              force: 0,
-              actions: routes.flat().map((action: Pool) => ({
-                pool_id: parseInt(action.pool_id),
-                token_in: action.token_in,
-                token_out: action.token_out,
-                amount_in: action.amount_in,
-                min_amount_out: action.min_amount_out,
-              })),
-            }),
-          },
-          gas: "180000000000000",
-          amount: "1",
-        },
-      ],
+    const swapTxns = await swapFromServer({
+      tokenIn: tokenInData,
+      tokenOut: tokenOutData,
+      amountIn: amountIn,
+      accountId: accountId,
+      swapsToDoServer: swapRes.result_data,
+  
     });
 
-    if (tokenOutData.id === WRAP_NEAR_CONTRACT_ID) {
-      const minAmountOut = percentLess(
-        0.01,
-        scientificNotationToString(receiveAmount.toString())
-      );
-
-      transactions.push(nearWithdrawTransaction(minAmountOut));
-    }
-
-    if (!tokenInStorage) {
-      transactions.unshift({
-        receiverId: tokenInData.id,
-        functionCalls: [registerAccountOnToken(accountId as string)],
-      });
-    }
-    if (!tokenOutStorage) {
-      transactions.unshift({
-        receiverId: tokenOutData.id,
-        functionCalls: [registerAccountOnToken(accountId as string)],
-      });
-    }
-
     return res.json({
-      transactions: transactions,
+      transactions: swapTxns,
       outEstimate: Big(receiveAmount).toFixed(5),
     });
   } catch (error) {
