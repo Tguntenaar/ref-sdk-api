954095c8aefda28d989262ae71a906bdc485b676
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-01-29 02:06:21 +0530

    increase rate limit to 180

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ const port = Number(process.env.PORT || 3000);
 
 const apiLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minutes
-  max: 100, // limit each IP to 100 requests per minute
+  max: 180, // limit each IP to 100 requests per minute
 });
 
 app.use(cors());
